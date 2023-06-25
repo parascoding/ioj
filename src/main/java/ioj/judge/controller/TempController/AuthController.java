4a113d3b8213e38fd69dd91dd410ae971123bf29
parascoding <parascoding@gmail.com>
2023-06-25 17:38:33 +0530

    Configured for railway deploy

--- src/main/java/ioj/judge/controller/TempController/AuthController.java
+++ src/main/java/ioj/judge/controller/TempController/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -114,6 +115,11 @@ public class AuthController {
     @ExceptionHandler(BadCredentialsException.class)
     public String exceptionHandler() {
         return "Credentials Invalid !!";
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "CHECK";
     }
 
 }
